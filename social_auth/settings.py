2d2ef77ebfc37ddd90eec24632de920c09baeb5a
Raj Sahu <80415025+RSahu20@users.noreply.github.com>
2024-05-13 00:32:10 +0530

    Update settings.py

--- social_auth/settings.py
+++ social_auth/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5(o9m$8g5ig_*a3kigp@vn)@-%xc-+shrj@ig_oa5^z(lk5@dd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
